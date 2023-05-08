cc22d49e330cf7ad08de5ba077847aa76941388d
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2023-05-08 21:44:40 +0900

    TimeManager member variable, function 선언하기

--- WindowEngine/TimgMgr.cpp
+++ WindowEngine/TimgMgr.cpp
@@ -1,10 +1,47 @@
 #include "pch.h"
 #include "TimgMgr.h"
 
-void TimgMgr::Init()
+TimgMgr::TimgMgr()
+	:m_curentCount{}
+	,m_previousCount{}
+	,m_frequency{}
+	,m_deltaTime{}
+	,m_accumulatedDeltaTime{}
+	,m_callCount{}
+	,m_FramePerSecond{}
 {
 }
 
-void TimgMgr::Update()
+TimgMgr::~TimgMgr()
 {
 }
+
+void TimgMgr::init()
+{
+}
+
+void TimgMgr::update()
+{
+}
+
+void TimgMgr::render()
+{
+}
+
+double TimgMgr::GetDeltaTime()
+{
+	return m_deltaTime;
+}
+
+UINT TimgMgr::GetFramePerSecond()
+{
+	return m_FramePerSecond;
+}
+// 
+// #ifdef _DEBUG
+// 
+// if (m_deltaTime > 0.1)
+// {
+// 	m_deltaTime = 0.1;
+// }
+// #endif
--- WindowEngine/TimgMgr.h
+++ WindowEngine/TimgMgr.h
@@ -5,10 +5,23 @@ class TimgMgr
 {
 	SINGLE(TimgMgr);
 
+private:
+	LARGE_INTEGER m_curentCount;
+	LARGE_INTEGER m_previousCount;
+	LARGE_INTEGER m_frequency;
+	
+	double m_deltaTime;
+	double m_accumulatedDeltaTime;
+
+	UINT m_callCount;
+	UINT m_FramePerSecond;
+
 public:
-	void Init();
-	void Update();
+	void init();
+	void update();
+	void render();
 
-
+	double GetDeltaTime();
+	UINT GetFramePerSecond();
 };
 
